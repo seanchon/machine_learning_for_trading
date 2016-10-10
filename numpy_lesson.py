f76b4606add641b27a2fc56881915be521a2a54f
Sean Chon <schon@gracenote.com>
2016-10-10 16:02:39 -0700

    01-03 Accessing array elements

--- numpy_lesson.py
+++ numpy_lesson.py
@@ -98,5 +98,24 @@ def test_run5():
     print("NumPy mean is {} times faster than manual for loops.".format(speedup))
 
 
+def test_run6():
+    a = np.random.rand(5, 4)
+    print("Array:\n{}".format(a))
+
+    # Accessing element at position (3, 2)
+    element = a[3, 2]
+    print(element)
+
+    # Slicing
+    # Elements in defined range
+    print(a[0, 1:3])
+
+    # Top left corner
+    print(a[0:2, 0:2])
+
+    # Note: Slice n:m:t specifies a range that starts at n, and stops before m, in steps of size t
+    print(a[:, 0:3:2])
+
+
 if __name__ == "__main__":
-    test_run5()
+    test_run6()
