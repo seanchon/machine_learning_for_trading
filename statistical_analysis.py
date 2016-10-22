028f5380074eb614420681ca0dc5b1386d67566f
Sean Chon <schon@gracenote.com>
2016-10-21 17:43:36 -0700

    01-04 Computing rolling statistics

--- statistical_analysis.py
+++ statistical_analysis.py
@@ -1,20 +1,36 @@
 from lib.get_data import get_data
 from lib.plot import plot_data
 from lib.time_function import how_long
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import time
 
 
 def test_run():
     # Read data
-    df = get_data(['SPY', 'XOM', 'GOOG', 'GLD'], '2010-01-01', '2012-12-31')
-    plot_data(df)
+    df = get_data(['SPY', 'XOM', 'GOOG', 'GLD'], '2012-01-01', '2012-12-31')
+    # plot_data(df)
 
     # Compute global statistics for each stock
-    print(df.mean())
-    print(df.median())
-    print(df.std())
+    # print(df.mean())
+    # print(df.median())
+    # print(df.std())
 
+    # Plot SPY data, retain matplotlib axis object
+    ax = df['SPY'].plot(title="SPY rolling mean", label='SPY')
+
+    # Compute rolling mean using a 20-day window
+    rm_SPY = pd.Series(df['SPY']).rolling(window=20).mean()
+
+    # Add rolling mean to same plot
+    rm_SPY.plot(label='Rolling mean', ax=ax)
+
+    # Add axis labels and legend
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    ax.legend(loc='upper left')
+    plt.show()
 
 if __name__ == "__main__":
     test_run()
