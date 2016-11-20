1c70f5f6f6b713b7663d535e9bc22796104770e2
Sean Chon <chonsean@gmail.com>
2016-11-20 11:29:35 -0800

    01-06 Scatterplots in python

--- 01-06-histograms_and_scatterplots.py
+++ 01-06-histograms_and_scatterplots.py
@@ -1,7 +1,8 @@
 """Plot a histogram."""
 
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 from lib.get_data import get_data
 from lib.plot import plot_data
@@ -46,5 +47,26 @@ def test_run():
     plt.legend(loc='upper right')
     plt.show()
 
+def test_run2():
+    # Read data
+    symbols = ['SPY', 'XOM', 'GOLD']
+    df = get_data(symbols, '2009-01-01', '2012-12-31')
+
+    # Compute daily returns
+    daily_returns = compute_daily_returns(df)
+
+    # Scatterplot SPY vs XOM
+    daily_returns.plot(kind='scatter', x='SPY', y='XOM')
+    beta_XOM, alpha_XOM = np.polyfit(daily_returns['SPY'], daily_returns['XOM'], 1)
+    plt.plot(daily_returns['SPY'], beta_XOM * daily_returns['SPY'] + alpha_XOM, '-', color='r')
+    plt.show()
+
+    # Scatterplot SPY vs XOM
+    daily_returns.plot(kind='scatter', x='SPY', y='GOLD')
+    beta_XOM, alpha_XOM = np.polyfit(daily_returns['SPY'], daily_returns['GOLD'], 1)
+    plt.plot(daily_returns['SPY'], beta_XOM * daily_returns['SPY'] + alpha_XOM, '-', color='r')
+    plt.show()
+
+
 if __name__ == '__main__':
-    test_run()
+    test_run2()
